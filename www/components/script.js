9c8d214b84807919464122209f75c391b88e6200
Chow0828 <viros123456789@gmail.com>
2021-11-30 08:24:07 +0900

    Project's Initial Commit

---
+++ www/components/script.js
@@ -0,0 +1,45 @@
+// This is a JavaScript file
+
+$(document).on('click', '#q1', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag2.html";
+});
+$(document).on('click', '#q2', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag3.html";
+});
+$(document).on('click', '#q3', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag4.html";
+});
+$(document).on('click', '#q4', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag5.html";
+});
+$(document).on('click', '#q5', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag6.html";
+});
+$(document).on('click', '#q6', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag7.html";
+});
+$(document).on('click', '#q7', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag8.html";
+});
+$(document).on('click', '#q8', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag9.html";
+});
+$(document).on('click', '#q9', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag10.html";
+});
+$(document).on('click', '#q10', function(){
+  alert("Resposta Correta! clique em ok para avançar");
+  window.location = "pag11.html";
+});
+$(document).on('click', '#0', function(){
+  alert("Resposta Errada! Tente Novamente!");
+});
